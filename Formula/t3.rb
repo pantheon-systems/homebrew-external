c6c87b1f3fc7e70aa0d94309d1b5572794fb89f2
Kevin Porras <kporras07@gmail.com>
2021-10-12 01:30:39 +0000

    t3 3.0.0-alpha11

--- Formula/t3.rb
+++ Formula/t3.rb
@@ -1,10 +1,10 @@
 class T3 < Formula
   desc "Terminus is Pantheon's Command-line Interface (CLI)"
   homepage "https://pantheon.io/terminus"
-  url "https://github.com/pantheon-systems/pantheon-cli/releases/download/3.0.0-alpha9/t3"
+  url "https://github.com/pantheon-systems/pantheon-cli/releases/download/3.0.0-alpha11/t3"
   license "MIT"
-  sha256 "0a8d7031231a22f2e57c77d21b114a562b292fdb2d502ae2fc60ea3e45e66a8a"
-  version "3.0.0-alpha9"
+  sha256 "1392ac48ce28e29041c12bd5e24d4ffa03bc81d39efb5bcd6224abcc7c327e8c"
+  version "3.0.0-alpha11"
 
   uses_from_macos "php"
 
