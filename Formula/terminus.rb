b99bb12d601eea3f2b6f1ee4f7ffc553df06b859
Kevin Porras <kporras07@gmail.com>
2021-10-15 12:49:24 -0600

    Add composer as optional dependency and reorder formula.

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -2,9 +2,11 @@ class Terminus < Formula
   desc "Terminus is Pantheon's Command-line Interface (CLI)"
   homepage "https://pantheon.io/terminus"
   url "https://github.com/pantheon-systems/terminus/releases/download/3.0.0-alpha9/terminus"
-  license "MIT"
-  sha256 "0a8d7031231a22f2e57c77d21b114a562b292fdb2d502ae2fc60ea3e45e66a8a"
   version "3.0.0-alpha9"
+  sha256 "0a8d7031231a22f2e57c77d21b114a562b292fdb2d502ae2fc60ea3e45e66a8a"
+  license "MIT"
+
+  depends_on "composer" => :optional
 
   uses_from_macos "php"
 
