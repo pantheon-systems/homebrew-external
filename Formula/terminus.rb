4a4fccc9be32995186148c448edfb6cfc64fd3fb
Kevin Porras <kporras07@gmail.com>
2021-10-18 16:41:04 -0600

    Update formula to 3.0.0-beta1.

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -1,9 +1,9 @@
 class Terminus < Formula
   desc "Terminus is Pantheon's Command-line Interface (CLI)"
   homepage "https://pantheon.io/terminus"
-  url "https://github.com/pantheon-systems/terminus/releases/download/3.0.0-alpha9/terminus"
-  version "3.0.0-alpha9"
-  sha256 "0a8d7031231a22f2e57c77d21b114a562b292fdb2d502ae2fc60ea3e45e66a8a"
+  url "https://github.com/pantheon-systems/terminus/releases/download/3.0.0-beta1/terminus"
+  version "3.0.0-beta1"
+  sha256 "e723a599e6c819381c3c50fe4917e243f134fd00a5b7b269ad9b79d8200761f9"
   license "MIT"
 
   depends_on "composer" => :optional
