77485144f0a155ccd9b8dcf05e9a155128952f86
Brian Weaver <brian.weaver@pantheon.io>
2021-12-03 16:42:45 -0500

    Re-ran release automation, new sha

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -3,7 +3,7 @@ class Terminus < Formula
   homepage "https://pantheon.io/terminus"
   url "https://github.com/pantheon-systems/terminus/releases/download/3.0.0-rc1/terminus"
   version "3.0.0-rc1"
-  sha256 "2740630b9279a88916fce23ad013748f10ccefc075b63a6c2919707601473873"
+  sha256 "704f22e1ee4c7bb327fc13b79a9568244807662d269e2228472c8f52281496d9"
   license "MIT"
 
   depends_on "composer" => :optional
