9f069b2c3dcf217ecf008a776a5222764af0adab
Brian Weaver <87093053+namespacebrian@users.noreply.github.com>
2021-12-03 19:50:55 -0500

    Revert "Update formula to 3.0.0-rc1"

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -1,9 +1,9 @@
 class Terminus < Formula
   desc "Terminus is Pantheon's Command-line Interface (CLI)"
   homepage "https://pantheon.io/terminus"
-  url "https://github.com/pantheon-systems/terminus/releases/download/3.0.0-rc1/terminus"
-  version "3.0.0-rc1"
-  sha256 "704f22e1ee4c7bb327fc13b79a9568244807662d269e2228472c8f52281496d9"
+  url "https://github.com/pantheon-systems/terminus/releases/download/3.0.0-beta1/terminus"
+  version "3.0.0-beta1"
+  sha256 "e723a599e6c819381c3c50fe4917e243f134fd00a5b7b269ad9b79d8200761f9"
   license "MIT"
 
   depends_on "composer" => :optional
