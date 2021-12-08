017f79a0fe3524dcbdbd8e2c69128c5720abc234
Brian Weaver <brian.weaver@pantheon.io>
2021-12-08 17:06:24 -0500

    Update terminus formula to 3.0.1

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -1,9 +1,9 @@
 class Terminus < Formula
   desc "Terminus is Pantheon's Command-line Interface (CLI)"
   homepage "https://pantheon.io/terminus"
-  url "https://github.com/pantheon-systems/terminus/releases/download/3.0.0/terminus"
+  url "https://github.com/pantheon-systems/terminus/releases/download/3.0.1/terminus.phar"
   version "3.0.0"
-  sha256 "4e7eacf633acce1e4503743b668b29993cebba11895369c897d5be21e839f43a"
+  sha256 "b3a444bcb5870cb9352f7708aa4e4212b4113ec6662b13cd123a6b0f2f958f40"
   license "MIT"
 
   depends_on "composer" => :optional
@@ -11,7 +11,7 @@ class Terminus < Formula
   uses_from_macos "php"
 
   def install
-    bin.install "terminus" => "terminus"
+    bin.install "terminus.phar" => "terminus"
   end
 
   test do
