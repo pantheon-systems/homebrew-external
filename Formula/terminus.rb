606f47876df4f435fe902f6b9e6e9024ccc4e40e
J Ryan Wagner <jryanwagner00@gmail.com>
2022-10-18 14:13:56 -0400

    Updates terminus to version 3.1.0

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -1,9 +1,9 @@
 class Terminus < Formula
   desc "Terminus is Pantheon's Command-line Interface (CLI)"
   homepage "https://pantheon.io/terminus"
-  url "https://github.com/pantheon-systems/terminus/releases/download/3.0.9/terminus.phar"
-  version "3.0.9"
-  sha256 "ac4e1d55f48b95beca2e536d875f5aa935d8de6b79fa5e8861a5103860e19a9b"
+  url "https://github.com/pantheon-systems/terminus/releases/download/3.1.0/terminus.phar"
+  version "3.1.0"
+  sha256 "83b4ef92b4ff5b835c91b10b223802fcd4bccbe87297bc9fcf3f883bcd7d4bf8"
   license "MIT"
 
   depends_on "composer" => :optional
