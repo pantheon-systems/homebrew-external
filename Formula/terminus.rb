5c01c803b98840274a498ed7c38503ef5f28eebf
John Spellman <john.spellman@pantheon.io>
2023-02-27 14:40:58 -0700

    Update formula to 3.1.3

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -1,9 +1,9 @@
 class Terminus < Formula
   desc "Terminus is Pantheon's Command-line Interface (CLI)"
   homepage "https://pantheon.io/terminus"
-  url "https://github.com/pantheon-systems/terminus/releases/download/3.1.2/terminus.phar"
-  version "3.1.2"
-  sha256 "bbcbc97f4ccb0a638df24bd35949c406b7e8a4ec456881c6f17b23bd34aeac73"
+  url "https://github.com/pantheon-systems/terminus/releases/download/3.1.3/terminus.phar"
+  version "3.1.3"
+  sha256 "e9c2be872753b6d4adbaefde44082ca817b97bb04f934053991d92f113a1a647"
   license "MIT"
 
   depends_on "composer" => :optional
