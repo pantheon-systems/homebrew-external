af27463b02278a636f22a574cb0a16d931c4bf40
Kevin Porras <kevin.porras@pantheon.io>
2023-03-01 15:24:17 -0600

    Remove version from formula.

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -2,7 +2,6 @@ class Terminus < Formula
   desc "Terminus is Pantheon's Command-line Interface (CLI)"
   homepage "https://pantheon.io/terminus"
   url "https://github.com/pantheon-systems/terminus/releases/download/3.1.3/terminus.phar"
-  version "3.1.3"
   sha256 "e9c2be872753b6d4adbaefde44082ca817b97bb04f934053991d92f113a1a647"
   license "MIT"
 
