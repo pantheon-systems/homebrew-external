b899b6a55de4dd99320f41f8795c9ed3918be8b8
Vitalii Kolodribskyi <vitaliikolodribskyi@pantheon.io>
2026-06-29 09:04:46 -0700

    fix: remove formula name from description to pass brew style check

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -1,5 +1,5 @@
 class Terminus < Formula
-  desc "Terminus is Pantheon's Command-line Interface (CLI)"
+  desc "Pantheon's Command-line Interface (CLI)"
   homepage "https://pantheon.io/terminus"
   url "https://github.com/pantheon-systems/terminus/releases/download/4.3.2/terminus.phar"
   sha256 "1eb7be556adba66bfb87d197211bf62671031e7294024c5a35dd8348b84f7aab"
